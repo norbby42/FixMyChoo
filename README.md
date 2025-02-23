50b181709403c88b87ab68216efe8473859b9f25
norbby42 <norbby42@gmail.com>
2025-02-23 00:39:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# FixMyChoo
+
+Describe your project here!
+
+## Using this Template
+
+Template Users: Make sure to remove this section after you create your project!
---
+++ src/FixMyChoo/settings/Settings.cs
@@ -0,0 +1,8 @@
+﻿using BepInEx.Configuration;
+
+namespace FixMyChoo.Settings;
+
+public class FixMyChooSettings(ConfigFile config)
+{
+    public ConfigEntry<bool> MySettingsBool = config.Bind<bool>("SectionName", "MySettingsBool", true, "This is an example boolean setting!");
+}
